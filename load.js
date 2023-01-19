d6cacb343927c1cea64a3f3ebcf17b79fc6f1807
Kimberley Ni <wukimberley98@gmail.com>
2023-01-19 10:33:58 -0500

    testing phaser

--- load.js
+++ load.js
@@ -1,1 +1,24 @@
-// when game initially loads
+// when game initially loads, run the following?
+var config = { // allows us to config the game
+    type: Phaser.AUTO, // falls back to canvas otherwise
+    width: 800, // centering
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('back', 'apocalypse2.jpg');
+}
+
+function create() {
+    this.add.image(400, 300, 'back');
+}
+
+function update() {
+}
