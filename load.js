2235c12564948d6504d745554d803cc813b44563
Kimberley Ni <wukimberley98@gmail.com>
2023-01-20 10:36:10 -0500

    putting stuff onto the screen

--- load.js
+++ load.js
@@ -1,8 +1,8 @@
 // when game initially loads, run the following?
 var config = { // allows us to config the game
     type: Phaser.AUTO, // falls back to canvas otherwise
-    width: 800, // centering
-    height: 600,
+    width: 1200, // centering
+    height: 660,
     physics: {
         default: 'arcade',
         arcade: {
@@ -15,6 +15,7 @@ var config = { // allows us to config the game
         create: create,
         update: update
     }
+    
 };
 
 var platforms;
@@ -22,15 +23,16 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('back', 'pictures/apocalypse2.jpg');
+    this.load.image('ground', 'pictures/platform.jpg');
 }
 
 function create() {
-    this.add.image(400, 300, 'back');
+    this.add.image(600, 330, 'back').setScale(3.6).setOrigin(.5,.5);
 
     // code to add platforming maybe? i need to do more research -K
-    // platforms = this.physics.add.staticGroup();
+    platforms = this.physics.add.staticGroup();
 
-    // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     // platforms.create(600, 400, 'ground');
     // platforms.create(50, 250, 'ground');
