ba033caf5f8bcb89e49bbc31ee2aaa7e3cff82b5
Kimberley Ni <wukimberley98@gmail.com>
2023-01-23 10:29:25 -0500

    some things to try to make the sprite show up

--- load.js
+++ load.js
@@ -15,7 +15,7 @@ var config = { // allows us to config the game
         create: create,
         update: update
     }
-    
+
 };
 
 // variables for players + platforms + game itself + controls
@@ -28,60 +28,78 @@ var jumpButton;
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('back', 'pictures/apocalypse2.jpg');
+    this.load.image('back', 'pictures/apocalypse1.jpg');
     this.load.image('ground', 'pictures/platform.jpg');
 
     // should be loading in sprite for one of the players (not working) - aj
-    this.load.image('player1', 'sprites/watergirl/Sprites/Idle.png')
+    this.load.spritesheet('wg', 'sprites/watergirl/Sprites/Idle.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
-    this.add.image(600, 330, 'back').setScale(3.6).setOrigin(.5,.5);
+    this.add.image(600, 330, 'back').setScale(1.2).setOrigin(.5, .5);
 
-    // code to add platforming maybe? i need to do more research -K
+    // code to add platforms
     platforms = this.physics.add.staticGroup();
-
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     // physics, fps, gravity
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.time.desiredFps = 30;
-    game.physics.arcade.gravity.y = 250;
+    // game.physics.startSystem(Phaser.Physics.ARCADE);
+    // game.time.desiredFps = 30;
+    // game.physics.arcade.gravity.y = 250;
     // should place sprites on screen (not working) also sould enable physics for player - aj
-    fireboy = game.add.sprite(32, 32, 'sprites/watergirl/Sprites/Idle.png');
-    // game.physics.enable(fireboy, Phaser.Physics.Arcade);
+    watergirl = this.physics.add.sprite(400, 568, 'wg');
+    
+    game.physics.enable(watergirl, Phaser.Physics.Arcade);
 
-    player.body.bounce.y = 0.2;
-    player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 16);
+    watergirl.setBounce(0.2);
+    watergirl.setCollideWorldBounds(true);
+
+    // should animate stuff
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('wg', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'wg', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('wg', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    watergirl.body.setSize(20, 32, 5, 16);
 
     // potentially use to make camera follow player around - aj
     // game.camera.follow(player);
 
-
-
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    // platforms.create(600, 400, 'ground');
-    // platforms.create(50, 250, 'ground');
-    // platforms.create(750, 220, 'ground');
+
+    this.physics.add.collider(player, platforms);
 }
 
 function update() {
     // should be covering basic left right movement + jumping - aj
-    player.body.velocity.x = 0;
+    watergirl.body.velocity.x = 0;
 
-   if (cursors.left.isDown) {
-       player.body.velocity.x = -150;
-   }
+    if (cursors.left.isDown) {
+        watergirl.body.velocity.x = -150;
+    }
 
-   else if (cursors.right.isDown) {
-       player.body.velocity.x = 150;
-   }
+    else if (cursors.right.isDown) {
+        watergirl.body.velocity.x = 150;
+    }
 
-   if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-    {
-        player.body.velocity.y = -250;
+    if (jumpButton.isDown && watergirl.body.onFloor() && game.time.now > jumpTimer) {
+        watergirl.body.velocity.y = -250;
         jumpTimer = game.time.now + 750;
     }
 }
